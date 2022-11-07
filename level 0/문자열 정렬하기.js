92c11119e06c4d68e273bbbf18bfd6cdddc2802a
hyozzi92 <gytjdzja@gmail.com>
2022-11-07 14:16:54 +0900

    문자열 정렬하기

---
+++ level 0/문자열 정렬하기.js
@@ -0,0 +1,16 @@
+//문제 설명
+//문자열 my_string이 매개변수로 주어질 때, my_string 안에 있는 숫자만 골라 오름차순 정렬한 리스트를 return 하도록 solution 함수를 작성해보세요.
+
+function solution(my_string) {
+  var answer = [];
+  var arr = my_string.split("");
+  var re = /[a-z]/;
+  var sum = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == Number(arr[i])) {
+      sum.push(parseInt(arr[i]));
+    }
+  }
+  answer = sum.sort();
+  return answer;
+}
