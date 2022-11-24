c8418b290c6ff51cc85b284ecd6bee2c53aa61c2
hyozzi92 <gytjdzja@gmail.com>
2022-11-24 13:15:38 +0900

    이진수 더하기

---
+++ level 0/이진수 더하기.js
@@ -0,0 +1,9 @@
+//문제 설명
+//이진수를 의미하는 두 개의 문자열 bin1과 bin2가 매개변수로 주어질 때, 두 이진수의 합을 return하도록 solution 함수를 완성해주세요.
+
+function solution(bin1, bin2) {
+  var answer = "";
+  var num = parseInt(bin1, 2) + parseInt(bin2, 2);
+  answer = num.toString(2);
+  return answer;
+}
