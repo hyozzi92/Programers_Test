f2bc8ed4d5615eb4889c412d1c4a66b9beac0b84
hyozzi92 <gytjdzja@gmail.com>
2022-12-08 16:28:57 +0900

    외계어 사전

---
+++ level 0/외계어 사전.js
@@ -0,0 +1,18 @@
+//문제 설명
+//PROGRAMMERS-962 행성에 불시착한 우주비행사 머쓱이는 외계행성의 언어를 공부하려고 합니다. 알파벳이 담긴 배열 spell과 외계어 사전 dic이 매개변수로 주어집니다.
+//spell에 담긴 알파벳을 한번씩만 모두 사용한 단어가 dic에 존재한다면 1, 존재하지 않는다면 2를 return하도록 solution 함수를 완성해주세요.
+
+function solution(spell, dic) {
+  var answer = 0;
+  console.log(dic[0].split("").sort().join(""));
+  console.log(spell.sort().join(""));
+  var arr1 = [];
+  var arr2 = spell.sort().join("");
+
+  for (let i = 0; i < dic.length; i++) {
+    arr1.push(dic[i].split("").sort().join(""));
+  }
+  arr1.includes(arr2) ? (answer = 1) : (answer = 2);
+
+  return answer;
+}
