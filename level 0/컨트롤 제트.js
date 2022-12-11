3af05b61edfbc9109eb225c89bd84f22daf5646a
hyozzi92 <gytjdzja@gmail.com>
2022-12-11 16:34:43 +0900

    컨트롤제트

---
+++ level 0/컨트롤 제트.js
@@ -0,0 +1,19 @@
+//숫자와 "Z"가 공백으로 구분되어 담긴 문자열이 주어집니다. 문자열에 있는 숫자를 차례대로 더하려고 합니다. 이 때 "Z"가 나오면 바로 전에 더했던 숫자를 뺀다는 뜻입니다. 숫자와 "Z"로 이루어진 문자열 s가 주어질 때, 머쓱이가 구한 값을 return 하도록 solution 함수를 완성해보세요.
+
+function solution(s) {
+  var answer = 0;
+  var arr = s.split(" ");
+
+  sum1 = 0;
+  sum2 = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== "Z") {
+      sum1 = sum1 + parseInt(arr[i]);
+    } else if (arr[i] == "Z") {
+      sum2 = sum2 + parseInt(arr[i - 1]);
+    }
+  }
+  answer = sum1 - sum2;
+  return answer;
+}
