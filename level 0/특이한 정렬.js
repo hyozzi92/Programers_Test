f51b6397d62e9d36367671170a70037e8ce6b542
hyozzi92 <gytjdzja@gmail.com>
2023-01-01 20:14:06 +0900

    'working'

--- level 0/특이한 정렬.js
+++ level 0/특이한 정렬.js
@@ -2,7 +2,7 @@ function solution(numlist, n) {
   var answer = [];
 
   for (let i = 0; i < numlist.length; i++) {
-    console.log(Math.abs(n - numlist[i]));
+    (Math.abs(n - numlist[i]));
   }
   //for문 말고 다른거 써보기
 
